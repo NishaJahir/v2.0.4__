d4e0fdba2f8628c774131080ffe12f9300cbb43f
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-04-12 15:18:07 +0530

    Update NovalnetSofortPaymentMethod.php

--- src/Methods/NovalnetSofortPaymentMethod.php
+++ src/Methods/NovalnetSofortPaymentMethod.php
@@ -82,7 +82,7 @@ class NovalnetSofortPaymentMethod extends PaymentMethodService
 		
 		$active_payment_allowed_country = 'true';
 		if ($allowed_country = $this->configRepository->get('Novalnet.novalnet_sofort_allowed_country')) {
-		$active_payment_allowed_country  = $this->paymentService->allowedCountrieslist($this->basket, $allowed_country);
+		$active_payment_allowed_country  = $this->paymentService->allowedCountries($this->basket, $allowed_country);
 		}
 	    
 	    $active_payment_minimum_amount = 'true';
