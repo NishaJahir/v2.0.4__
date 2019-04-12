5d9d31df658820cdc63aa77e7607bf5ccf330da2
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-04-12 15:02:35 +0530

    Update NovalnetIdealPaymentMethod.php

--- src/Methods/NovalnetIdealPaymentMethod.php
+++ src/Methods/NovalnetIdealPaymentMethod.php
@@ -21,6 +21,8 @@ use Plenty\Modules\Payment\Method\Contracts\PaymentMethodService;
 use Plenty\Plugin\Application;
 use Novalnet\Helper\PaymentHelper;
 use Novalnet\Services\PaymentService;
+use Plenty\Modules\Basket\Models\Basket;
+use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
 
 /**
  * Class NovalnetIdealPaymentMethod
@@ -43,6 +45,11 @@ class NovalnetIdealPaymentMethod extends PaymentMethodService
 	 * @var PaymentService
 	 */
 	private $paymentService;
+	
+	/**
+     * @var Basket
+     */
+    private $basket;
 
     /**
      * NovalnetPaymentMethod constructor.
@@ -53,11 +60,13 @@ class NovalnetIdealPaymentMethod extends PaymentMethodService
      */
     public function __construct(ConfigRepository $configRepository,
                                 PaymentHelper $paymentHelper,
-                                PaymentService $paymentService)
+                                PaymentService $paymentService,
+			       BasketRepositoryContract $basket)
     {
         $this->configRepository = $configRepository;
         $this->paymentHelper = $paymentHelper;
         $this->paymentService = $paymentService;
+	    $this->basket = $basket->load();
     }
 
     /**
@@ -68,11 +77,30 @@ class NovalnetIdealPaymentMethod extends PaymentMethodService
      */
     public function isActive():bool
     {
+       if ($this->configRepository->get('Novalnet.novalnet_ideal_payment_active') == 'true') {
+		
 		$active_payment_allowed_country = 'true';
 		if ($allowed_country = $this->configRepository->get('Novalnet.novalnet_ideal_allowed_country')) {
-		$active_payment_allowed_country  = $this->paymentService->allowedCountries($allowed_country);
+		$active_payment_allowed_country  = $this->paymentService->allowedCountrieslist($this->basket, $allowed_country);
 		}
-        return (bool)(($this->configRepository->get('Novalnet.novalnet_ideal_payment_active') == 'true') && $this->paymentHelper->paymentActive() && $active_payment_allowed_country);
+	    
+	    $active_payment_minimum_amount = 'true';
+	    $minimum_amount = trim($this->configRepository->get('Novalnet.novalnet_ideal_minimum_order_amount'));
+	    if (!empty($minimum_amount) && is_numeric($minimum_amount)) {
+		$active_payment_minimum_amount = $this->paymentService->getMinBasketAmount($this->basket, $minimum_amount);
+		}
+		
+		$active_payment_maximum_amount = 'true';
+	    $maximum_amount = trim($this->configRepository->get('Novalnet.novalnet_ideal_maximum_order_amount'));
+	    if (!empty($maximum_amount) && is_numeric($maximum_amount)) {
+		$active_payment_maximum_amount = $this->paymentService->getMaxBasketAmount($this->basket, $maximum_amount);
+		}
+	    
+	    
+        return (bool)($this->paymentHelper->paymentActive() && $active_payment_allowed_country && $active_payment_minimum_amount && $active_payment_maximum_amount);
+        } 
+        return false;
+    
     }
 
     /**
