8408e7e1956a80a6c3f674b9c840b381f6500068
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-04-12 12:14:59 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -869,4 +869,37 @@ class PaymentService
 		return false;
 	}
 	
-}
+	
+	public function getMinBasketAmount(Basket $basket, $minimum_amount) {
+	
+	if (! is_null($basket) && $basket instanceof Basket) {
+	$amount = $this->paymentHelper->ConvertAmountToSmallerUnit($basket->basketAmount);
+	if (!empty($minimum_amount) && $minimum_amount<$amount)	{
+		return 'true';
+	}
+	} 
+	return false;
+	}
+	
+	
+	public function allowedCountrieslist(Basket $basket, $allowed_country) {
+		$allowed_country = str_replace(' ', '', strtoupper($allowed_country));
+		$allowed_country_array = explode(',', $allowed_country);	
+		
+			if (! is_null($basket) && $basket instanceof Basket) {
+			if(!empty($basket->customerInvoiceAddressId)){
+				$billingAddressId = $basket->customerInvoiceAddressId;				
+				$address = $this->addressRepository->findAddressById($billingAddressId);
+				if(!empty($address)){
+					$country = $this->countryRepository->findIsoCode($address->countryId, 'iso_code_2');
+					if(!empty($country)){
+						if (in_array ($country, $allowed_country_array)) {
+							return true;
+						}  
+					}
+				}
+			}
+		
+		}
+		return false;
+	}
